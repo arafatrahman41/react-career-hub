8572551b7d3a7bd87f678b48272ca260be8e1971
arafatrahman41 <arafatrahman41@outlook.com>
2024-05-19 21:10:44 +0600

    need to fixed so many thing

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -5,6 +5,20 @@ import { getStoredJobApplication } from "../../utils/localstorage";
 const AppliedJobs = () => {
   const jobs = useLoaderData();
   const [appliedJobs, setAppliedJobs] = useState([]);
+  const [displayJobs, setDisplayJobs] = useState([]);
+
+  const handleJobsFilter = filter => {
+    if(filter === 'all'){
+      setDisplayJobs(appliedJobs);
+    }else if(filter === 'remote'){
+      const remoteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Remote');
+      setDisplayJobs(remoteJobs)
+    }else if(filter === 'onsite'){
+      const onsiteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Onsite');
+      setDisplayJobs(onsiteJobs)
+    }
+  }
+
   useEffect(() => {
     const storedJobId = getStoredJobApplication();
     if (jobs.length) {
@@ -16,12 +30,36 @@ const AppliedJobs = () => {
           JobsApplied.push(job);
         }
       }
-      setAppliedJobs(JobsApplied)
+      setAppliedJobs(JobsApplied);
+      setDisplayJobs(JobsApplied);
     }
-  }, []);
+  }, [jobs]);
   return (
     <div>
       <h2>Jobs I applied: {appliedJobs.length}</h2>
+      <details className="dropdown">
+        <summary className="m-1 btn">open or close</summary>
+        <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+          <li onClick={() => handleJobsFilter('all')}>
+            <a>All</a>
+          </li>
+          <li onClick={() => handleJobsFilter('remote')}>
+            <a>Remote</a>
+          </li>
+          <li onClick={() => handleJobsFilter('onsite')}>
+            <a>Onsite</a>
+          </li>
+        </ul>
+      </details>
+      <ul>
+        {
+        displayJobs.map((job) => (
+          <li key={job.id}>
+            <span></span>
+            {job.job_title} {job.company_name}: {job.remote_or_onsite}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
